f86dc894b1fa3891dea86ec0a84d2c512a9cb7a5
gon2gon2 <ark5354@gmail.com>
2022-05-08 16:01:00 +0900

    fix: segmentation fault because of ',' at line 31

--- mm.c
+++ mm.c
@@ -28,7 +28,7 @@ team_t team = {
     /* First member's full name */
     "gon2gon2",
     /* First member's email address */
-    "ark5354@gmail.com"
+    "ark5354@gmail.com",
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */
